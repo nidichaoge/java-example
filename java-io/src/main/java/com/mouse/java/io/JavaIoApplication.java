e1f79da09cf124c5289a49b841270cf3727bf19e
houwenchao <houwenchao@shuwen.com>
2020-01-20 15:37:24 +0800

    init project

---
+++ java-io/src/main/java/com/mouse/java/io/JavaIoApplication.java
@@ -0,0 +1,13 @@
+package com.mouse.java.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaIoApplication.class, args);
+    }
+
+}
---
+++ java-io/src/test/java/com/mouse/java/io/JavaIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouse.java.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
